4da8b43a941b5879a6200d0424b8ee9f5896553e
Natalie Tsvetkova <natalie.tsvetkova@mongodb.com>
2017-06-21 16:44:51 -0400

    made a little repl

--- gobucks.go
+++ gobucks.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"strconv"
+	//"gopkg.in/mgo.v2"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Double yay gobucks")
+	repl("natalie")
+}
+
+func repl(user string) {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print(user + "> ")
+	for scanner.Scan() {
+		input := scanner.Text()
+		parse(input)
+		fmt.Print(user + "> ")
+	}
+}
+
+func parse(input string) {
+	arr := strings.Split(input, " ")
+	switch arr[0] {
+	case "gamble":
+		if len(arr) < 2 {
+			errorMessage()
+		}
+		numGamble, err := strconv.Atoi(arr[1])
+		if err != nil {
+			errorMessage()
+		}
+		fmt.Println(numGamble)
+	default:
+		errorMessage()
+	}
+}
+
+func errorMessage() {
+	fmt.Println("please enter 'gamble <number>'")
 }
